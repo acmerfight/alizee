1a4d9c2a5bc72ab566964b531704e6637dd9a13e
acmerfight <acmerfight@gmail.com>
2015-01-03 17:32:21 +0800

    add basic re2post

--- parse.py
+++ parse.py
@@ -23,22 +23,71 @@ class Tokenizer(object):
         return this
 
 
-class Parser(object):
+class MarkStruct(object):
+
+    def __init__(self, atom_number=0, alt_number=0):
+        self.atom_number = atom_number
+        self.alt_number = alt_number
+
+
+class ReParser(object):
+    """
+    将正则表达式转换为后缀表达式
+    """
 
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
         self.tokens_stack = Stack()
-        self.atom_number = 0
         self.alt_number = 0
+        self.atom_number = 0
+        self.mark_struct = MarkStruct()
+        self.mark_list = []
 
     def parse(self):
         while 1:
             char = self.tokenizer.get()
-            if char == "(":
+            if char is None:
+                break
+            elif char == "(":
+                if self.atom_number > 1:
+                    self.atom_number -= 1
+                    self.tokens_stack.push(".")
+                self.mark_struct.alt_number = self.alt_number
+                self.mark_struct.atom_number = self.atom_number
+                self.mark_list.append(self.mark_struct)
+                self.mark_struct = MarkStruct()
+                self.alt_number = 0
+                self.atom_number = 0
                 continue
-            elif char in {'a', 'b'}:
+            elif char == ")":
+                while self.atom_number - 1 > 0:
+                    self.tokens_stack.push(".")
+                    self.atom_number -= 1
+                while self.alt_number > 0:
+                    self.tokens_stack.push("|")
+                    self.alt_number -= 1
+                mark_struct = self.mark_list.pop()
+                self.alt_number = mark_struct.alt_number
+                self.atom_number = mark_struct.atom_number
+                self.atom_number += 1
+                continue
+            elif char == "|":
+                while self.atom_number - 1 > 0:
+                    self.tokens_stack.push(".")
+                self.alt_number += 1
+                continue
+            elif char in {"*", "+", "?"}:
                 self.tokens_stack.push(char)
                 continue
-            elif char in {'*', '?', '+'}:
+            else:
+                if self.atom_number > 1:
+                    self.atom_number -= 1
+                    self.tokens_stack.push('.')
                 self.tokens_stack.push(char)
+                self.atom_number += 1
                 continue
+
+tokenizer = Tokenizer("(ab)*c")
+parser = ReParser(tokenizer)
+parser.parse()
+print parser.tokens_stack
